cec116c040dac726e4cfdf96f572467851775cbb
joinwithyogeshRK <joinwithyogeshRK@users.noreply.github.com>
2025-08-21 16:12:14 +0530

    Upload src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { TodoProvider } from './context/TodoContext';
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <TodoProvider>
+      <TodoApp />
+    </TodoProvider>
+  );
+}
+
+export default App;
